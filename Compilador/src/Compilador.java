883ac424c1937d1da9a63b12e40903bcf64b8d8e
Unknown <leonardo98ti@gmail.com>
2018-08-17 16:49:12 -0300

    Projeto do compilador

---
+++ Compilador/src/Compilador.java
@@ -0,0 +1,13 @@
+
+import java.io.IOException;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.RecognitionException;
+
+public class Compilador {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+    }
+}
